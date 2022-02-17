1ccfec81e721ddf85793be05c6394a5597fcd363
TheEppicJR <mantacast4154@gmail.com>
2022-02-16 22:12:34 -0800

    idk now to cpp lol

--- src/navigation/cone_estimation/src/cone_estimation_node.cpp
+++ src/navigation/cone_estimation/src/cone_estimation_node.cpp
@@ -19,40 +19,49 @@
 
 
 
-
 using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
 
-class MinimalPublisher : public rclcpp::Node
+class ConeEstimator : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
+    ConeEstimator()
+    : Node("cone_estimation")
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      publisher_ = this->create_publisher<driverless_msgs::msg::PointWithCovarianceStampedArray>("cone_estimation/estimations", 10);
+      lidar_subscription_ = this->create_subscription<std_msgs::msg::String>("cone_estimation/lidar_cones", 10, std::bind(&ConeEstimator::topic_callback, this, _1));
+      vision_subscription_ = this->create_subscription<std_msgs::msg::String>("cone_estimation/vision_cones", 10, std::bind(&ConeEstimator::topic_callback, this, _1));
+      odom_subscription_ = this->create_subscription<std_msgs::msg::String>("state_estimation/odom", 10, std::bind(&ConeEstimator::topic_callback, this, _1)); // Need to find the right kind of msg for this
+      timer_ = this->create_wall_timer(500ms, std::bind(&ConeEstimator::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
       auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
+      message.data = "Hello, world! ";
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
+
+    void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+    {
+      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    }
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
+
+
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<ConeEstimator>());
   rclcpp::shutdown();
   return 0;
 }
