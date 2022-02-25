Covariance works waaaaayyyyyy better now and frame timing is correct, still puts a non existant cone on the inside of a corner every once in a while

src/perception/cone_pipeline/cone_pipeline/node_cone_pipe.py
@@ -32,7 +32,8 @@ from transforms3d.euler import quat2euler
 # import required sub modules
 from .point import PointWithCov
 from . import kmeans_clustering as km
-from . import kdtree
+from .kdtree import create, KDNode
+from .kdtree import Node as kdNode
 
 # initialise logger
 LOGGER = logging.getLogger(__name__)
@@ -70,9 +71,13 @@ class ConePipeline(Node):
 
     def getNearestOdom(self, stamp):
         # need to switch this over to a position from a EKF with covariance
-        # have to do the time stuff this way because the compating of time in the message_filters __init__.py is wack
-        locodom = self.actualodom.getElemBeforeTime(Time(seconds=stamp.sec, nanoseconds=stamp.nanosec, clock_type=ClockType.ROS_TIME))
+        locodom: Odometry = self.actualodom.getElemAfterTime(Time.from_msg(stamp))
         cov = np.identity(3) * 0.0025 # standin covariance for ekf assuming the variance is sigma = 5cm with no covariance
+        # if the nearest Odom in the cache is more than 0.05 sec off then just throw it away
+        if locodom is not None:
+            #print(Time.from_msg(stamp) - Time.from_msg(locodom.header.stamp))
+            if Time.from_msg(stamp) - Time.from_msg(locodom.header.stamp) > Duration(nanoseconds=0.05*(10**9)):
+                return None, cov
         return locodom, cov
 
     def fuseCone(self, point):
@@ -95,7 +100,7 @@ class ConePipeline(Node):
             # see if it is close enough for our liking (ths distance in m) than we will turn it into a actual cone
             if closestcone[0][0].data.inTwoSigma(point):
                 # select the first group from the returned tuple (point objects) and then get the first one (which will be our point since we only asked for one)
-                pointnew = closestcone[0][0]
+                pointnew: kdNode = closestcone[0][0]
                 # fuse the points together
                 pointnew.data.update(point)
                 # if there is already a tree of Offical Cones tm than add it to that tree and then rebalance it
@@ -104,7 +109,7 @@ class ConePipeline(Node):
                     self.conesKDTree.rebalance()
                 # if not than make a tree for them
                 else:
-                    self.conesKDTree = kdtree.create([point])
+                    self.conesKDTree = create([point])
                 # remove the point from the buffer tree
                 self.bufferKDTree.remove(pointnew.data)
                 # the rebalance the buffer tree since we removed something
@@ -115,7 +120,7 @@ class ConePipeline(Node):
                 self.bufferKDTree.rebalance()
         # if we dont already have a buffer tree than create one (this should only ever happen once i hope, otherwise something has gone horrible wrong)
         else:
-            self.bufferKDTree = kdtree.create([point])
+            self.bufferKDTree = create([point])
 
     def fusePoints(self, points):
         # if we have cones in the cone tree than check if we can fuse our new points with them
@@ -187,7 +192,7 @@ class ConePipeline(Node):
         # need to make a section to remove old points from the buffer
         if self.bufferKDTree is not None and self.bufferKDTree.data is not None:
             for point in self.bufferKDTree.returnElements():
-                if self.get_clock().now() - Duration(nanoseconds=2*10**9) > Time(seconds=point.header.stamp.sec, nanoseconds=point.header.stamp.nanosec, clock_type=ClockType.ROS_TIME):
+                if self.get_clock().now() - Duration(nanoseconds=2*10**9) > Time.from_msg(point.header.stamp):
                     self.bufferKDTree.remove(point)
             self.bufferKDTree.rebalance()
         if self.conesKDTree is not None and self.conesKDTree.data is not None:
@@ -203,6 +208,7 @@ class ConePipeline(Node):
                         self.conesKDTree.remove(point)
                         self.conesKDTree.rebalance()
                 if point.global_z < 0.2 or point.global_z > 0.65:
+                    #print(point.global_z)
                     self.conesKDTree.remove(point)
                     self.conesKDTree.rebalance()
 

src/perception/cone_pipeline/cone_pipeline/node_track_server.py
@@ -32,7 +32,7 @@ from scipy.spatial import Delaunay
 
 # import required sub modules
 from .point import PointWithCov, Edge, Triangle, Point
-from . import kdtree
+from .kdtree import create, KDNode
 
 # initialise logger
 LOGGER = logging.getLogger(__name__)
@@ -137,7 +137,7 @@ class ConePipeline(Node):
             coneList.append(PointWithCov(0, 0, 0, None, cone.color, cone.header, cone.position.x, cone.position.y, cone.position.z, np.array(cone.covariance).reshape((3,3))))
 
         tri = Delaunay(conePoints)
-        self.coneKDTree = kdtree.create(coneList)
+        self.coneKDTree = create(coneList)
         
         for simp in tri.simplices:
             p1 = self.coneKDTree.search_knn(Point(conePoints[simp[0]][0], conePoints[simp[0]][1]), 1)[0][0].data
@@ -150,8 +150,8 @@ class ConePipeline(Node):
 
         self.triangleList: List[Triangle] = triangleList
         self.edgeList: List[Edge] = edgeList
-        self.edgeKDTree = kdtree.create(edgeList)
-        self.triangleKDTree = kdtree.create(triangleList)
+        self.edgeKDTree = create(edgeList)
+        self.triangleKDTree = create(triangleList)
         return True
 
 

src/perception/lidar_pipeline/lidar_pipeline/node_simple.py
@@ -15,6 +15,7 @@ from driverless_msgs.msg import Cone, ConeDetectionStamped, PointWithCovarianceS
 import time
 from typing import List
 import sys
+from math import sin, cos, sqrt, atan2
 import numpy as np
 
 # import ROS function that has been ported to ROS2 by
@@ -23,6 +24,14 @@ from .scripts.read_pcl import read_points_list
 # lidar cone detection algorithm
 from .scripts.sim_simple import find_cones
 
+def cone_cov(cov, x, y, z):
+    bearing = atan2(y, x)
+    distance = sqrt(x**2 +y**2 + z**2)
+    s, c = sin(bearing), cos(bearing)
+    rotation_matrix = np.array([[c, -1*s, 0],[s, c, 0], [0, 0, 1]])
+    new_cov = rotation_matrix @ cov @ rotation_matrix.T
+    retcov = new_cov * (distance/20)
+    return retcov
 
 def cone_msg(x_coord: float, y_coord: float, z_coord: float) -> Cone: 
     # {Cone.YELLOW, Cone.BLUE, Cone.ORANGE_SMALL}
@@ -67,7 +76,7 @@ class LidarProcessing(Node):
         self.detection_publisher: Publisher = self.create_publisher(ConeDetectionStamped, "lidar/cone_detection", 1)
         self.detection_publisher_cov: Publisher = self.create_publisher(PointWithCovarianceStampedArray, "/lidar/cone_detection_cov", 1)
 
-        self.lidarcov = np.array([[ 0.02,  0.1, 0], [ 0.1, 0.02, 0.1], [0, 0.1,  0.01]])
+        self.lidarcov = np.array([[ 0.04,  0, 0], [ 0, 0.06, 0], [0, 0,  0.02]])
 
         self.get_logger().info('---LiDAR sim processing node initialised---')
 
@@ -98,7 +107,8 @@ class LidarProcessing(Node):
 
         for cone in cones:
             detected_cones.append(cone_msg(cone[0], cone[1], cone[2]))
-            detected_cones_cov.append(cone_msg_cov(cone[0], cone[1], cone[2], 4, self.lidarcov.flatten(), pc2_msg.header))
+            conecov = cone_cov(self.lidarcov, cone[0], cone[1], cone[2])
+            detected_cones_cov.append(cone_msg_cov(cone[0], cone[1], cone[2], 4, conecov.flatten(), pc2_msg.header))
        
         detection_msg = ConeDetectionStamped(
             header=pc2_msg.header,

src/perception/vision_pipeline/vision_pipeline/node_detector.py
@@ -69,6 +69,14 @@ def cone_distance(
 
     return np.mean(depth_roi[conePts]) / 2
 
+def cone_cov(cov, bearing, distance, elevation):
+    # could also impliment shifting cov due to elevation
+    s, c = sin(radians(bearing)), cos(radians(bearing))
+    rotation_matrix = np.array([[c, -1*s, 0],[s, c, 0], [0, 0, 1]])
+    new_cov = rotation_matrix @ cov @ rotation_matrix.T
+    retcov = new_cov * (distance/20)
+    return retcov
+
 def cone_bearing(
     colour_frame_cone_bounding_box: Rect,
     colour_frame_camera_info: CameraInfo,
@@ -148,12 +156,9 @@ class DetectorNode(Node):
         super().__init__("cone_detector")
 
         # subscribers
-        # colour_sub = message_filters.Subscriber(self, Image, "/zed2i/zed_node/rgb/image_rect_color")
-        # self.colour_cache = message_filters.Cache(colour_sub, 10)
-        depth_sub = message_filters.Subscriber(self, Image, "/fsds/camera/depth_registered")
-        self.depth_cache = message_filters.Cache(depth_sub, 20)
-        # self.create_subscription(CameraInfo, "/zed2i/zed_node/rgb/camera_info", self.callback, 10)
-        self.create_subscription(Image, "/fsds/camera/image_rect_color", self.callback, 10)
+        image_sub = message_filters.Subscriber(self, Image, "/fsds/camera/image_rect_color")
+        self.image_cache = message_filters.Cache(image_sub, 20)
+        self.create_subscription(Image, "/fsds/camera/depth_registered", self.callback, 10)
 
 
         # publishers
@@ -161,7 +166,7 @@ class DetectorNode(Node):
         self.detection_publisher_cov: Publisher = self.create_publisher(PointWithCovarianceStampedArray, "/vision/cone_detection_cov", 1)
         self.debug_img_publisher: Publisher = self.create_publisher(Image, "/vision/debug_img", 1)
 
-        self.visioncov = np.array([[ 0.4,  0.01, 0], [ 0.01, 0.1, 0], [0, 0,  0.01]])
+        self.visioncov = np.array([[ 0.4,  0, 0], [ 0, 0.1, 0], [0, 0,  0.05]])
 
         # set which cone detection this will be using
         self.get_logger().info("Selected detection mode. 0==cv2, 1==torch, 2==trt")
@@ -170,12 +175,12 @@ class DetectorNode(Node):
         self.get_bounding_boxes_callable = get_bounding_boxes_callable
 
 
-    def callback(self, colour_msg: Image):
-        stamp = colour_msg.header.stamp
+    def callback(self, depth_msg: Image):
+        stamp = depth_msg.header.stamp
         colour_camera_info_msg = CameraInfo()
         colour_camera_info_msg.height = 360
         colour_camera_info_msg.width = 640
-        depth_msg = self.depth_cache.getElemAfterTime(Time(seconds=stamp.sec, nanoseconds=stamp.nanosec, clock_type=ClockType.ROS_TIME) - Duration(nanoseconds=0.02*10**9))
+        colour_msg: Image = self.image_cache.getElemAfterTime(Time.from_msg(stamp) - Duration(nanoseconds=0.02*10**9))
         logger = self.get_logger()
         logger.debug("Received image")
 
@@ -183,6 +188,7 @@ class DetectorNode(Node):
             print("No camera")
             return None
 
+        #print(f"{Time.from_msg(stamp)-Time.from_msg(colour_msg.header.stamp)}")
         start: float = time.time() # begin a timer
 
         colour_frame: np.ndarray = cv_bridge.imgmsg_to_cv2(colour_msg, desired_encoding='bgra8')
@@ -209,8 +215,9 @@ class DetectorNode(Node):
 
             bearing = cone_bearing(bounding_box, colour_camera_info_msg)
             elevation = cone_elevation(bounding_box, colour_camera_info_msg)
+            conecov = cone_cov(self.visioncov, bearing, distance, elevation)
             detected_cones.append(cone_msg(distance, bearing, elevation, cone_colour))
-            detected_cones_cov.append(cone_msg_cov(distance, bearing, elevation, cone_colour, self.visioncov.flatten(), colour_msg.header))
+            detected_cones_cov.append(cone_msg_cov(distance, bearing, elevation, cone_colour, conecov.flatten(), colour_msg.header))
             draw_box(colour_frame, box=bounding_box, colour=display_colour, distance=distance)
 
         detection_msg = ConeDetectionStamped(