65117deeb1adec410ed2534d26503d7c5d6c09be
TheEppicJR <mantacast4154@gmail.com>
2022-03-08 23:40:31 -0800

    I regret my decision to use Rust a bit

--- src/perception/cone_pipeline_rs/src/markermaker.rs
+++ src/perception/cone_pipeline_rs/src/markermaker.rs
@@ -1,14 +1,18 @@
+use super::edgefinder;
+use super::pathperimitives;
+use nalgebra as na;
+use pathperimitives::{
+    DirectedEdgeHandleType, EdgeColor, FaceType, PointColor, PointF2, TileType, Track,
+    Triangulation, UndirectedEdgeType, VertexType,
+};
 use r2r;
 use r2r::driverless_msgs::msg::PointWithCovariance;
 use r2r::driverless_msgs::msg::PointWithCovarianceArrayStamped;
-use r2r::visualization_msgs::msg::{Marker, MarkerArray}
-use r2r::geometry_msgs::msg::{Point}
-use r2r::std_msgs::msg::{ColorRGBA}
+use r2r::geometry_msgs::msg::Point;
 use r2r::nav_msgs::msg::Odometry;
+use r2r::std_msgs::msg::ColorRGBA;
+use r2r::visualization_msgs::msg::{Marker, MarkerArray};
 use r2r::QosProfile;
-use nalgebra as na;
-mod edgefinder;
-mod pathperimitives;
 
 pub fn generate_edges(triangulation_obj: &Triangulation) -> Marker {
     let mut marker_points: Vec<Point>;
@@ -23,28 +27,52 @@ pub fn generate_edges(triangulation_obj: &Triangulation) -> Marker {
         // }
         for point in edge.vertices() {
             let color_rgba = get_color_RGBA(&point.data);
-            let point = get_point_msg(&point.position)
+            let point = get_point_msg(&point.position);
         }
     }
 }
 
 fn get_point_msg(point: &PointF2) -> Point {
-    return Point { x: point.x, y: point.y }
+    return Point {
+        x: point.x,
+        y: point.y,
+        z: 0.0,
+    };
 }
 
 fn get_color_RGBA(conevertex: &VertexType) -> ColorRGBA {
     match conevertex.color {
         PointColor::Blue => {
-            return ColorRGBA { r: 0.0, g: 0.0, b: 1.0, a: 1.0}
+            return ColorRGBA {
+                r: 0.0,
+                g: 0.0,
+                b: 1.0,
+                a: 1.0,
+            }
         }
         PointColor::Yellow => {
-            return ColorRGBA { r: 1.0, g: 1.0, b: 0.0, a: 1.0}
+            return ColorRGBA {
+                r: 1.0,
+                g: 1.0,
+                b: 0.0,
+                a: 1.0,
+            }
         }
         PointColor::SmallOrange | PointColor::BigOrange => {
-            return ColorRGBA { r: 1.0, g: 0.7, b: 0.0, a: 1.0}
+            return ColorRGBA {
+                r: 1.0,
+                g: 0.7,
+                b: 0.0,
+                a: 1.0,
+            }
         }
         PointColor::Unknown => {
-            return ColorRGBA { r: 0.5, g: 0.5, b: 0.5, a: 1.0}
+            return ColorRGBA {
+                r: 0.5,
+                g: 0.5,
+                b: 0.5,
+                a: 1.0,
+            }
         }
     }
 }
