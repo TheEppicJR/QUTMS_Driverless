fe81bb46eeb6ca3988bf86731ceb331d55a918a2
TheEppicJR <mantacast4154@gmail.com>
2022-03-22 12:41:44 -0700

    arg

--- src/hardware/sr_can/sr_can/camera_node.py
+++ src/hardware/sr_can/sr_can/camera_node.py
@@ -41,6 +41,7 @@ class CameraNode(Node):
     def pubImage(self):
         ret, frame = self.cap.read()
         #self.cap.release()
+        print(type(img))
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         self.img_publisher.publish(cv_bridge.cv2_to_imgmsg(img, encoding="bgr8"))
 
