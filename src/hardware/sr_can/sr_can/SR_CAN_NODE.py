3300c67fbd5408ef3bcb9e362b48cbc9a0913e51
TheEppicJR <mantacast4154@gmail.com>
2022-03-29 21:30:19 -0700

    Changed it so the peak uses can1

--- src/hardware/sr_can/sr_can/SR_CAN_NODE.py
+++ src/hardware/sr_can/sr_can/SR_CAN_NODE.py
@@ -116,7 +116,7 @@ class SR_CAN(Node):
     def __init__(self):
         super().__init__("sr_can")
 
-        bus = Bus(interface='socketcan', channel='vcan0', receive_own_messages=False)
+        bus = Bus(interface='socketcan', channel='can1', receive_own_messages=False)
 
         channel_descripts, self.addys, rate, self.msgdats = gcp()
 
